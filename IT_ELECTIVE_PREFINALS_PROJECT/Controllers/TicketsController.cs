1b4914e14eb6ac232cf43118b3f4c9f1d3ef1c66
Sho Uno Sabesaje <155436471+ShoIchiiii@users.noreply.github.com>
2026-08-21 21:28:55 +0800

    Update TicketsController.cs

--- IT_ELECTIVE_PREFINALS_PROJECT/Controllers/TicketsController.cs
+++ IT_ELECTIVE_PREFINALS_PROJECT/Controllers/TicketsController.cs
@@ -1,10 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IT_ELECTIVE_PREFINALS_PROJECT.Data;
 
 namespace IT_ELECTIVE_PREFINALS_PROJECT.Controllers
 {
-    public class TicketsController
+    public class TicketsController : Controller
     {
+        private readonly HelpDeskContext _context;
+
+        public TicketsController(HelpDeskContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tickets
+        public async Task<IActionResult> Index()
+        {
+            var tickets = await _context.Tickets
+                .Include(t => t.Customer)
+                .Include(t => t.Category)
+                .OrderByDescending(t => t.CreatedAt)
+                .ToListAsync();
+
+            return View(tickets);
+        }
+
+        // GET: Tickets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Tickets
+                .Include(t => t.Customer)
+                .Include(t => t.Category)
+                .Include(t => t.TicketAssignments)
+                    .ThenInclude(ta => ta.Employee)
+                .Include(t => t.TicketTags)
+                    .ThenInclude(tt => tt.Tag)
+                .FirstOrDefaultAsync(m => m.TicketId == id);
+
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
     }
 }
