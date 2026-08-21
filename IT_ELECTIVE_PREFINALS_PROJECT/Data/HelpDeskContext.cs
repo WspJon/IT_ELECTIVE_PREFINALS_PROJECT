190e83432d2e13010c6b4db8baefd942ed214712
WspJon <realesjonnidel@gmail.com>
2026-08-21 16:32:10 +0800

    Setup HelpDeskContext and register SQLite DbContext in DI

---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Data/HelpDeskContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IT_ELECTIVE_PREFINALS_PROJECT.Models;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Data
+{
+    public class HelpDeskContext : DbContext
+    {
+        public HelpDeskContext(DbContextOptions<HelpDeskContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<TicketStatus> TicketStatuses { get; set; }
+        public DbSet<TicketPriority> TicketPriorities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           
+            modelBuilder.Entity<TeamMember>()
+                .HasKey(tm => new { tm.TeamId, tm.EmployeeId });
+        }
+    }
+}
