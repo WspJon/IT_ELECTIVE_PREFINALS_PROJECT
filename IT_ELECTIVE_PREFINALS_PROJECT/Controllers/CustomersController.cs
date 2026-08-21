6462c781f75f5b91526258abb96249f322a3c824
WspJon <realesjonnidel@gmail.com>
2026-08-21 16:47:29 +0800

    Build Customers Controller and Index view

---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IT_ELECTIVE_PREFINALS_PROJECT.Data;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly HelpDeskContext _context;
+
+        public CustomersController(HelpDeskContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _context.Customers
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+
+            return View(customers);
+        }
+    }
+}
