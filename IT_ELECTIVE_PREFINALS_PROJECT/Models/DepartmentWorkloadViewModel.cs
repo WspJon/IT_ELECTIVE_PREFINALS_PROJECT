ee7dbbb0ec1f98fe28e5f11c8950b185a4830fec
Sho Uno Sabesaje <155436471+ShoIchiiii@users.noreply.github.com>
2026-08-21 22:04:04 +0800

    feat(queries): add Department Workload query, ViewModel, and view

---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Models/DepartmentWorkloadViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Models
+{
+    internal class DepartmentWorkloadViewModel
+    {
+    }
+}
