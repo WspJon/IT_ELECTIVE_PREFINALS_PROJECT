fa48c79efbe10784f34a69b749a56a744bd6f611
Sho Uno Sabesaje <155436471+ShoIchiiii@users.noreply.github.com>
2026-08-21 21:01:52 +0800

    feat(tickets): add TicketsController and Index list view with status badges

---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Controllers/TicketsController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Controllers
+{
+    internal class TicketsController
+    {
+    }
+}
